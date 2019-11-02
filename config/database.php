4b9c76250f3b5e325fbe5af550d3e200367969f0
gisnasp <gisnasp2106@gmail.com>
2019-11-02 13:26:50 +0700

    first commit

--- config/database.php
+++ config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('db4free.net'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'agisna'),
+            'username' => env('DB_USERNAME', 'agisna21'),
+            'password' => env('DB_PASSWORD', 'agisna0621'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
